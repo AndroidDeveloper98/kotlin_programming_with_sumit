04d96b332c889e87da29f13a4798958f407a4b4b
Sumit Ojha <122687829+AndroidDeveloper98@users.noreply.github.com>
2024-09-01 21:53:27 +0530

    Triangle and pyramid pattern

---
+++ src/programs/basic/ReverseStromgMain.kt
@@ -0,0 +1,14 @@
+package programs.basic
+
+/**
+ * Program to reverse string.
+ */
+fun main() {
+    print("Enter name to reverse : ")
+    val name = readln()
+    var result = ""
+    name.forEachIndexed { index, c ->
+        result += name[name.length-1-index]
+    }
+    println("Reversed name : $result")
+}
---
+++ src/programs/basic/TriangleStarMain.kt
@@ -0,0 +1,24 @@
+package programs.basic
+
+fun main() {
+    println("===========Star triangle============")
+    for (i in 0..7) {
+        for (j in 0..i) {
+            print(" * ")
+        }
+        println()
+    }
+    println("===========Star triangle============")
+    println()
+    println("===========Star Pyramid============")
+    for(i in 0..<7){
+        for(j in 0..<7-i){
+            print(" ")
+        }
+        for(k in 0..<i){
+            print("* ")
+        }
+        println()
+    }
+    println("===========Star Pyramid============")
+}
